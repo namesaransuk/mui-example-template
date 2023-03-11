4ddb411d2f4f7c55b1a2f3891a0ce80d0eb6e454
namesaransuk <namesaransuk@gmail.com>
2023-03-11 16:57:14 +0700

    update edit cell in data grid success

--- src/pages/excel/index.js
+++ src/pages/excel/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import { read, utils, writeFile } from 'xlsx';
@@ -11,111 +11,12 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
 import Alert from '@mui/material/Alert';
-import { DataGrid } from '@mui/x-data-grid';
-
-// const fileTypes = ['xlsx', 'xls'];
-const useFakeMutation = () => {
-    return useCallback(
-        (excelList) =>
-            new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    if (excelList.name?.trim() === '') {
-                        reject(new Error("Error while saving excelList: name can't be empty."));
-                    } else {
-                        resolve({ ...excelList, name: excelList.name?.toUpperCase() });
-                    }
-                }, 200);
-            }),
-        []
-    );
-};
-
-function computeMutation(newRow, oldRow) {
-    if (newRow.name !== oldRow.name) {
-        return `Name from '${oldRow.name}' to '${newRow.name}'`;
-    }
-    // if (newRow.age !== oldRow.age) {
-    //     return `Age from '${oldRow.age || ''}' to '${newRow.age || ''}'`;
-    // }
-    return null;
-}
+import { DataGrid, GridToolbar, GridCellEditCommitParams } from '@mui/x-data-grid';
 
 const Excel = () => {
     const [data, setData] = useState([]);
     const [excelList, setExcelList] = useState([]);
     const [lastRefresh, setLastRefresh] = useState(new Date());
-
-    const mutateRow = useFakeMutation();
-    const noButtonRef = useRef(null);
-    const [promiseArguments, setPromiseArguments] = useState(null);
-
-    const [snackbar, setSnackbar] = useState(null);
-
-    const handleCloseSnackbar = () => setSnackbar(null);
-
-    const processRowUpdate = useCallback(
-        (newRow, oldRow) =>
-            new Promise((resolve, reject) => {
-                const mutation = computeMutation(newRow, oldRow);
-                if (mutation) {
-                    // Save the arguments to resolve or reject the promise later
-                    setPromiseArguments({ resolve, reject, newRow, oldRow });
-                } else {
-                    resolve(oldRow); // Nothing was changed
-                }
-            }),
-        []
-    );
-
-    const handleNo = () => {
-        const { oldRow, resolve } = promiseArguments;
-        resolve(oldRow); // Resolve with the old row to not update the internal state
-        setPromiseArguments(null);
-    };
-
-    const handleYes = async () => {
-        const { newRow, oldRow, reject, resolve } = promiseArguments;
-
-        try {
-            // Make the HTTP request to save in the backend
-            const response = await mutateRow(newRow);
-            setSnackbar({ children: 'User successfully saved', severity: 'success' });
-            resolve(response);
-            setPromiseArguments(null);
-        } catch (error) {
-            setSnackbar({ children: "Name can't be empty", severity: 'error' });
-            reject(oldRow);
-            setPromiseArguments(null);
-        }
-    };
-
-    const handleEntered = () => {
-        // The `autoFocus` is not used because, if used, the same Enter that saves
-        // the cell triggers "No". Instead, we manually focus the "No" button once
-        // the dialog is fully open.
-        // noButtonRef.current?.focus();
-    };
-    const renderConfirmDialog = () => {
-        if (!promiseArguments) {
-            return null;
-        }
-
-        const { newRow, oldRow } = promiseArguments;
-        const mutation = computeMutation(newRow, oldRow);
-
-        return (
-            <Dialog maxWidth="xs" TransitionProps={{ onEntered: handleEntered }} open={!!promiseArguments}>
-                <DialogTitle>Are you sure?</DialogTitle>
-                <DialogContent dividers>{`Pressing 'Yes' will change ${mutation}.`}</DialogContent>
-                <DialogActions>
-                    <Button ref={noButtonRef} onClick={handleNo}>
-                        No
-                    </Button>
-                    <Button onClick={handleYes}>Yes</Button>
-                </DialogActions>
-            </Dialog>
-        );
-    };
 
     useEffect(() => {
         axios
@@ -207,28 +108,58 @@ const Excel = () => {
 
     const columns = [
         { field: 'id', headerName: '#', width: 70 },
-        { field: 'id_mt', headerName: 'ID', width: 70 },
-        { field: 'name', headerName: 'Name', flex: 1, editable: true },
-        { field: 'remain', headerName: 'Remain', type: 'number', width: 130 },
-        { field: 'unit', headerName: 'Unit', width: 130 }
+        { field: 'id_mt', headerName: 'ID', width: 70, editable: true },
+        {
+            field: 'name',
+            headerName: 'Name',
+            flex: 1,
+            editable: true
+        },
+        { field: 'remain', headerName: 'Remain', type: 'number', width: 130, editable: true },
+        { field: 'unit', headerName: 'Unit', width: 130, editable: true }
     ];
 
-    // const handleEditCellChangeCommitted = (params) => {
-    //     console.log('handleEditCellChangeCommitted called');
-    //     const { id, field, value } = params.props;
-    //     const newData = [...excelList];
-    //     const index = newData.findIndex((item) => item.id === id);
-    //     newData[index][field] = value;
-    //     // setData(newData);
-    //     axios
-    //         .put('http://localhost/react-api/' + id, newData[index])
-    //         .then((response) => {
-    //             console.log(response.data);
-    //         })
-    //         .catch((error) => {
-    //             console.error(error);
-    //         });
-    // };
+    const handleEditCellChangeCommitted = (params) => {
+        console.log(params);
+        axios
+            .put('http://localhost/react-api/excel.php', {
+                id: params.id,
+                value: params.value,
+                field: params.field
+            })
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+        // console.log(params);
+
+        // async function updateData() {
+        //     const data = {
+        //         id: params.id,
+        //         value: params.value,
+        //         field: params.field
+        //     };
+
+        //     const response = await fetch('http://localhost/react-api/excel.php', {
+        //         method: 'PUT',
+        //         headers: {
+        //             'Content-Type': 'application/json'
+        //         },
+        //         body: JSON.stringify(data)
+        //     });
+
+        //     if (response.ok) {
+        //         const result = await response.json();
+        //         console.log(result);
+        //     } else {
+        //         console.error('Error:', response.statusText);
+        //     }
+        // }
+
+        // updateData();
+    };
 
     return (
         <>
@@ -261,33 +192,26 @@ const Excel = () => {
             </div>
             <div className="row">
                 <div className="col-sm-12 offset-3">
-                    <div style={{ height: 400, width: '100%' }}>
-                        {renderConfirmDialog()}
+                    <div style={{ width: '100%' }}>
                         <DataGrid
+                            autoHeight
+                            editable
                             rows={excelList}
                             columns={columns}
                             pageSize={5}
                             rowsPerPageOptions={[5]}
-                            processRowUpdate={processRowUpdate}
+                            components={{
+                                Toolbar: GridToolbar
+                            }}
+                            onCellEditCommit={(params: GridCellEditCommitParams, event: MuiEvent) => {
+                                handleEditCellChangeCommitted(params);
+                            }}
+                            // onCellEditStart={handleCellEditStart}
+                            // onCellEditStop={handleEditCellChangeCommitted}
                         />
-                        {!!snackbar && (
-                            <Snackbar open onClose={handleCloseSnackbar} autoHideDuration={6000}>
-                                <Alert {...snackbar} onClose={handleCloseSnackbar} />
-                            </Snackbar>
-                        )}
                     </div>
                 </div>
             </div>
-            {/* <FileUploader
-                handleChange={handleChange}
-                name="file"
-                types={fileTypes}
-                className="custom-file-input"
-                id="inputGroupFile"
-                required
-                handleImport={handleImport}
-                accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-            /> */}
         </>
     );
 };
